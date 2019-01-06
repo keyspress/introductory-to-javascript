499067491e80a77bef1b70eae0e3d30d9a468119
Kyle Desktop <kylespresser@gmail.com>
2019-01-05 23:51:11 -0500

    lecture 17

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
-var hotText = 'MDN';
-var URL = 'https://developer.mozilla.org/';
+alert('Welcome to the page');
+var name = prompt('What is your name');
+var color = prompt('What is your favorite color?');
+var wtf = prompt('What is the air speed velocity of an unladen swallow?');
 
-document.write('Click to return to ' + hotText.link(URL));
+document.write('Hello ' + name + '. Your favorite color is ' + color);
+console.log('Program complete');
