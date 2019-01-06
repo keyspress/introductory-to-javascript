ff2a3c98b99b7583b0fa04f924161615633c54d4
Kyle Desktop <kylespresser@gmail.com>
2019-01-06 00:05:05 -0500

    lecture 23

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
-var random = Math.floor(Math.random() * 50) + 1;
+var number = prompt('pick a number');
+
+number = parseInt(number);
+
+var random = Math.floor(Math.random() * number + 1);
 
 alert(random);
